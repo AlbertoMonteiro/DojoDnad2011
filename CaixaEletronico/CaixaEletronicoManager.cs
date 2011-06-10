fe7115dedcb0433244af748bfbb4c73e6b03ec42
AlbertoMonteiro <betimbrasil@gmail.com>
2011-06-10 05:41:40 -0700

    Simplificação da lógica

--- CaixaEletronico/CaixaEletronicoManager.cs
+++ CaixaEletronico/CaixaEletronicoManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,7 +5,7 @@ namespace CaixaEletronico
 {
     public class CaixaEletronicoManager
     {
-        private int[] _notas = { 100, 50, 20, 10, 5, 2 };
+        private readonly int[] _notas = { 2, 5, 10, 20, 50, 100 };
 
         public int[] Sacar(int value)
         {
@@ -15,20 +14,12 @@ namespace CaixaEletronico
             while (valor > 0)
             {
                 var notas = _notas.Where(x => x <= valor).ToArray();
-                var primeira = (((valor - notas[0]) % 2) == 1);
-                var segunda = DivisorDe10(valor);
-                int n = primeira && segunda ? notas[1] : notas[0];
+                var primeira = valor % 5 != 0;
+                int n = primeira ? notas[0] : notas[notas.Length - 1];
                 valor = valor - n;
                 lista.Add(n);
             }
             return lista.OrderByDescending(x => x).ToArray();
-        }
-
-        private bool DivisorDe10(int valor1)
-        {
-            var a = ((valor1 % 5 == 3) || (valor1 % 5 == 1));
-            var b = new[] { 10, 5, 1, 2, 0 }.Contains((valor1 - (valor1 % 5)) / 10);
-            return a && b;
         }
     }
 }
